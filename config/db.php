c7310568902fdfb6c9dfb514e55f5344c94279f1
Ilyas <ilyas.nassiri@ifosup.wavre.be>
2024-06-14 15:23:34 +0200

    connexion db en ligne

--- config/db.php
+++ config/db.php
@@ -1,16 +1,24 @@
 <?php
-$host = "sql304.infinityfree.com";
-$db_name = "if0_36730779_app_ilyas";
-$username = "if0_36730779";
-$password = "OpSDbetMxdfpHO";
+// Configuration de la connexion à la base de données
+class Database {
+    private $host = "sql304.infinityfree.com";
+    private $db_name = "if0_36730779_app_ilyas";
+    private $username = "if0_36730779";
+    private $password = "OpSDbetMxdfpHO";
+    public $conn;
 
-try {
-    // Établir la connexion à la base de données avec PDO
-    $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
-    $conn->exec("set names utf8");
-} catch (PDOException $exception) {
-    echo "Connection error: " . $exception->getMessage();
+    public function getConnection(){
+        $this->conn = null;
+        try {
+            // Établir la connexion à la base de données avec PDO
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
 }
-
+?>
 
 
